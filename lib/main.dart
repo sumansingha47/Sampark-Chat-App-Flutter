869169f8f832395050dbb28383a44e4bc0d83c0c
SUMAN SINGHA <sumansingha047@gmail.com>
2024-12-09 23:02:12 +0530

    Start working on authentication functionality with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sampark_chat_app_24/Config/PagePath.dart';
@@ -6,8 +7,13 @@ import 'package:sampark_chat_app_24/Pages/Auth/AuthPage.dart';
 import 'package:sampark_chat_app_24/Pages/Home/HomePage.dart';
 import 'package:sampark_chat_app_24/Pages/SplashPage/SplashPage.dart';
 import 'package:sampark_chat_app_24/Pages/Welcome/WelcomePage.dart';
+import 'package:sampark_chat_app_24/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
